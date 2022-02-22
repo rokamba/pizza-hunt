a83f5dd1cbf75ea62afde87e2b5e633cc6416c03
rokamba <rokamba92@gmail.com>
2022-02-21 23:03:25 -0600

    added the comment model

--- models/pizza.js
+++ models/pizza.js
@@ -1,4 +1,6 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
 
 const PizzaSchema = new Schema(
   {
@@ -11,6 +13,7 @@ const PizzaSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
       type: String,
@@ -27,6 +30,7 @@ const PizzaSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true
     },
     id: false
   }
